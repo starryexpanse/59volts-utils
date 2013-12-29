8b59e2a28d787dc4fb54967ecf262f4dd87d96f4
Phil Peterson <ironmagma@dinwiddie.dreamhost.com>
2013-12-29 14:01:56 -0800

    Add WIP Weekly Email Sender

---
+++ Automated/WeeklyEmailSender/sendemail.php
@@ -0,0 +1,27 @@
+<?php
+
+// require('include/random_catchphrase.php');
+
+
+// $suffix = "";
+// 
+// $cap = getCatchphrase();
+// if ($cap) {
+// 
+// }
+
+
+$mesg = <<<MESSG
+It's time for our weekly check-in! Everyone, post what you've done this week!
+
+- MagBot
+MESSG;
+
+$supersecret = trim(file_get_contents('include/emailaddr.conf'));
+
+$additional_headers = 'Content-type: text/plain; charset=utf-8' . "\r\n"
+. 'From: MagBot <no-reply@starryexpanse.com>'
+. "Reply-To: Starry Expanse Group <$supersecret>";
+
+mail($supersecret, 'Weekly Check-in Email Thread', $mesg, $additional_headers);
+
