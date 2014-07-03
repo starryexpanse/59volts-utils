21d9b9053e823d7f6a58194b8607abe55a81102e
Philip Peterson <philip.c.peterson@gmail.com>
2014-07-03 00:31:53 -0400

    IRC logs backup cron job

---
+++ Automated/CronJobs/irclogs-backup.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+cd /var/backups/
+mkdir -p db/irclogs
+cd $_
+
+source ./Credentials/irclogs-backup.creds.sh
+
+OWNER=root
+GROUP=staff
+
+PREFIX="irclogs-"
+
+datetime="$(date +%Y%m%d)"
+cleanOld="python /usr/bin/59volts-utils/Scripts/cleanOld.py"
+
+ab_loc="$PREFIX""$datetime"
+mysqldump IrcLogs ab_logs -u "$DB_USER" -p "$DB_PASS" > "$ab_loc".sql
+tar cvzf "$ab_loc".tgz "$ab_loc".sql
+
+if [[ $? = 0 ]]; then
+    unlink "$ab_loc".sql
+    chown "$OWNER":"$GROUP" "$ab_loc".tgz
+else
+    echo 'Tar failed! (exit code '"$?"'). Out of disk space?'
+    chown "$OWNER":"$GROUP" "$ab_loc".tgz
+fi
+
+stem="$(pwd)/$PREFIX"
+$cleanOld "${stem}*.sql"
+$cleanOld "${stem}*.tgz"
